7d4557b3e67b331f0e4d1eb314daa99e0073277f
Karen Vu <karenkv@uci.edu>
2020-11-24 00:55:21 -0800

    Removed express server and et up spotify auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
+import {authEndpoint, clientId, redirectUri, scopes} from "./config";
 import logo from './assets/logo.svg';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            token: null
+            token: null,
         }
     }
 
     handleLogin = () => {
-
+        const toString = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20")}&response_type=token&show_dialog=true`
+        window.open(toString, "_self");
     }
 
     render() {
@@ -21,7 +24,7 @@ class App extends Component {
                   <img src={logo} alt={"logo"}/>
                </div>
                <div className={"body"}>
-                  <button onClick={this.handleLogin}>Login to Spotify</button>
+                   {!this.state && <button onClick={this.handleLogin}>Login to Spotify</button>}
                </div>
            </div>
         );
